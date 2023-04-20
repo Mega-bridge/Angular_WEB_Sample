3eefea503010c46cf4db8d61dbf2dbb78f6fb9dc
jeyelin <yerin08@megabridge.co.kr>
2023-04-20 18:26:29 +0900

    가족 관계, 성별, 직업 조회 서비스 추가

--- src/shared/service/mind-reader-control.service.ts
+++ src/shared/service/mind-reader-control.service.ts
@@ -3,6 +3,9 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {MrFamilyCodeResponse} from "../model/response/mr-family-code.response.model";
 import {MrObjectImageResponse} from "../model/response/mr-object-image.response.model";
+import {MrGenderCodeResponse} from "../model/response/mr-gender-code.response.model";
+import {MrJobCodeResponse} from "../model/response/mr-job-code.response.model";
+import {MrFamilyRelationCodeResponse} from "../model/response/mr-family-relation-code.response.model";
 
 @Injectable({
     providedIn: 'root'
@@ -26,9 +29,31 @@ export class MindReaderControlService {
     }
 
     /**
+     * 가족 관계 list 불러오기
+     */
+    getFamilyRelation(): Observable<MrFamilyRelationCodeResponse[]> {
+        return this.http.get<MrFamilyRelationCodeResponse[]>(`${this.MR_CONTROL_URL}/familyRelation`);
+    }
+
+    /**
      * object 정보 불러오기
      */
     getObjectData(): Observable<MrObjectImageResponse[]> {
         return this.http.get<MrObjectImageResponse[]>(`${this.MR_CONTROL_URL}/objectImage`);
     }
+
+    /**
+     * 성별 list 불러오기
+     */
+    getGender(): Observable<MrGenderCodeResponse[]> {
+        return this.http.get<MrGenderCodeResponse[]>(`${this.MR_CONTROL_URL}/gender`);
+    }
+
+    /**
+     * 직업 list 불러오기
+     */
+    getJob(): Observable<MrJobCodeResponse[]> {
+        return this.http.get<MrJobCodeResponse[]>(`${this.MR_CONTROL_URL}/job`);
+    }
 }
+
