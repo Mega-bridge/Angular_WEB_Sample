c567e15aa9818a169910661ea7fc3da40443b594
jeyelin <yerin08@megabridge.co.kr>
2023-04-28 13:13:10 +0900

    내담자 추가 입력 정보 생성 api 작업

--- src/app/input-info/input-info.component.ts
+++ src/app/input-info/input-info.component.ts
@@ -6,6 +6,9 @@ import {MindReaderControlService} from "../../shared/service/mind-reader-control
 import {MrFamilyRelationCodeResponse} from "../../shared/model/response/mr-family-relation-code.response.model";
 import {MrGenderCodeResponse} from "../../shared/model/response/mr-gender-code.response.model";
 import {MrJobCodeResponse} from "../../shared/model/response/mr-job-code.response.model";
+import {PatientInfoRequest} from "../../shared/model/request/patient-info.request.model";
+import {HttpErrorResponse} from "@angular/common/http";
+import {AlertService} from "../../shared/service/alert.service";
 
 @Component({
     selector: 'app-input-info',
@@ -22,11 +25,17 @@ export class InputInfoComponent implements OnInit{
     // 선택한 가족 리스트
     public selectedFamily: any[]=[];
     // 가족 관계 리스트
-    public familyRelation: MrFamilyRelationCodeResponse[] = []
+    public familyRelation: MrFamilyRelationCodeResponse[] = [];
     // 성별 리스트
     public genderList: MrGenderCodeResponse[] = [];
     // 직업 리스트
     public jobList: MrJobCodeResponse[] = [];
+    // 선택한 가족 리스트
+    public selectedFamilyList: any[]=[];
+    // 선택한 가족관게 리스트
+    public selectedFamilyRelation: string[] = [];
+    // 라디오 버튼 선택 저장
+    public selectedValues: string[] = [];
 
     /**
      * 생성자
@@ -35,22 +44,21 @@ export class InputInfoComponent implements OnInit{
      */
     constructor(
         private mindReaderControlService:MindReaderControlService,
-        private router: Router
+        private router: Router,
+        private alertService: AlertService,
 
     ) {
     }
 
     // input form
     public infoForm: FormGroup = new FormGroup({
-        email: new FormControl(""),
-        name: new FormControl(""),
-        sex: new FormControl(0),
-        age: new FormControl(0),
-        job: new FormControl(0),
-        familyNum: new FormControl(0),
-        familyRelationCode: new FormControl(0),
+        userEmail: new FormControl(''),
+        userName: new FormControl(''),
+        age: new FormControl(),
+        familyNum: new FormControl(),
+        jobCode: new FormControl(),
+        genderCode: new FormControl(),
     });
-
 
     ngOnInit() {
         // 가족 리스트 조회
@@ -59,7 +67,7 @@ export class InputInfoComponent implements OnInit{
                 next: async (data) => {
                     if (data){
                         console.log(data);
-                        this.familyTypeList = data;
+                        this.familyTypeList  = data
                     }
                 }
             });
@@ -68,7 +76,7 @@ export class InputInfoComponent implements OnInit{
             .subscribe({
                 next: async (data) => {
                     if (data){
-                        this.familyRelation = data;
+                        this.familyRelation  = data
                     }
                 }
             });
@@ -78,7 +86,7 @@ export class InputInfoComponent implements OnInit{
             .subscribe({
                 next: async (data) => {
                     if (data){
-                        this.genderList = data;
+                        this.genderList=data
                     }
                 }
             });
@@ -88,9 +96,42 @@ export class InputInfoComponent implements OnInit{
             .subscribe({
                 next: async (data) => {
                     if (data){
-                        this.jobList = data;
+                        this.jobList = data
                     }
                 }
+            });
+    }
+
+    /**
+     * 추가 정보 기입하기
+     */
+    patientInfo(){
+        this.familyInfo()
+        let familyRelation1=this.selectedFamilyRelation.join(',');
+        let familyInfo1=this.selectedFamilyList.join(',');
+
+        const request: PatientInfoRequest = {
+            id: 0,
+            age: Number(this.infoForm.controls['age'].value),
+            familyInfo: familyInfo1,
+            familyNum: Number(this.infoForm.controls['familyNum'].value),
+            familyRelation: familyRelation1,
+            genderId: this.infoForm.controls['genderCode'].value.id,
+            jobId: this.infoForm.controls['jobCode'].value.id,
+            userEmail: this.infoForm.controls['userEmail'].value,
+            userName: this.infoForm.controls['userName'].value,
+        }
+        console.log(request)
+        // 내담자 추가 정보 생성
+        this.mindReaderControlService.postPatientInfo(request)
+            .subscribe({
+                next: async (data) => {
+                    if(data) {
+                        console.log(data);
+                        this.startLogin();
+                    }
+                },
+                error: (err: HttpErrorResponse) => this.alertService.openAlert(err.message)
             });
     }
 
@@ -107,6 +148,31 @@ export class InputInfoComponent implements OnInit{
         } else {
             this.selectedFamilyType.push(selected);
         }
+    }
+
+    updateSelectedValue(event:any){
+        // if (event.target.checked) {
+        //     this.radioValues.push(event);
+        // } else {
+        //     const index = this.radioValues.indexOf(event);
+        //     if (index !== -1) {
+        //         this.radioValues.splice(index, 1);
+        //     }
+        // }
+
+    }
+
+    /**
+     * 가족 데이터
+     */
+    familyInfo(){
+        this.selectedValues = this.selectedValues.filter(item => item !== null);
+        this.selectedValues.forEach(item => {
+            const splitted = item.split('_');
+            this.selectedFamilyList.push(splitted[0]);
+            this.selectedFamilyList.join(',');
+            this.selectedFamilyRelation.push(splitted[1]);
+        });
     }
 
 
