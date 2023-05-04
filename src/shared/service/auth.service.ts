39896d6f7cba0ad08a17902c69bfa77cff7d5ead
kwonseoyoon <seoyoon24@megabridge.co.kr>
2023-05-04 19:34:41 +0900

    로그인 정보 다시 sessionStorage로 변경,,^^

--- src/shared/service/auth.service.ts
+++ src/shared/service/auth.service.ts
@@ -50,21 +50,21 @@ export class AuthService {
      * login한 email 저장
      */
     setUserEmail(email: string){
-        localStorage.setItem(this.USER_EMAIL,email);
+        sessionStorage.setItem(this.USER_EMAIL,email);
     }
 
     /**
      * login한 email 가져오기
      */
     getUserEmail(){
-        return localStorage.getItem(this.USER_EMAIL);
+        return sessionStorage.getItem(this.USER_EMAIL);
     }
 
     /**
      * token 정보 조회
      */
     getToken() {
-        return localStorage.getItem(this.TOKEN_NAME);
+        return sessionStorage.getItem(this.TOKEN_NAME);
     }
 
     /**
@@ -72,21 +72,21 @@ export class AuthService {
      * @param token
      */
     setToken(token: string): void {
-        localStorage.setItem(this.TOKEN_NAME, token);
+        sessionStorage.setItem(this.TOKEN_NAME, token);
     }
 
     /**
      * logout 시 저장되어 있는 세션 정보 삭제
      */
     removeToken(): void {
-        localStorage.removeItem(this.TOKEN_NAME);
-        localStorage.removeItem(this.USER_EMAIL);
+        sessionStorage.removeItem(this.TOKEN_NAME);
+        sessionStorage.removeItem(this.USER_EMAIL);
     }
 
 
     // 토큰 유효성 검증
     isAuthenticated(): boolean {
-        const token = localStorage.getItem(this.TOKEN_NAME);
+        const token = sessionStorage.getItem(this.TOKEN_NAME);
         return token ? !this.tokenExpired(token) : false;
     }
 
